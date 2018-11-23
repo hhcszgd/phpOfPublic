7c1dd166f451957da5e454a8428a6ee34c976a37
WY <wy@WYdeMac-mini.local>
2018-11-23 20:33:56 +0800

    test jsonencode

--- ConnectMysql.php
+++ ConnectMysql.php
@@ -11,5 +11,6 @@ $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
     die("连接失败: " . $conn->connect_error);
 }
-echo "连接成功";
+$str = "连接成功"
+echo $str.json_encode();
 ?>
